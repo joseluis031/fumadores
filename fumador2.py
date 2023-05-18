68531b29b34e4cca2610cc17028ed2ab82dab154
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-05-18 09:39:45 +0200

    corregir errores

--- fumador2.py
+++ fumador2.py
@@ -26,5 +26,31 @@ class Fumador(threading.Thread):
                 condicion2.notify()
                 
                 
+class Agente(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        
+    def run(self):
+        while True:
+            with condicion2:
+                condicion2.wait()
+                print("\nEl agente esta poniendo los ingredientes")
+                time.sleep(1)
+                condicion1.notify()
+                condicion1.notify()
+                condicion1.notify()
+                print("\nEl agente ha puesto los ingredientes")
+                time.sleep(1)
 
-                
+def main():
+    
+    fumadores = [Fumador(ingrediente[0]), Fumador(ingrediente[1]), Fumador(ingrediente[2]), Fumador(ingrediente[3]), Fumador(ingrediente[4])]
+    agente = Agente()
+    
+    for fumador in fumadores:
+        fumador.start()
+    
+    agente.start()
+
+if __name__ == "__main__":
+    main()
