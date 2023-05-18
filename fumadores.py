74f698fee147123bbbfbde86d6a42d1d2f0f4db4
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-05-18 09:08:30 +0200

    Update fumadores.py

--- fumadores.py
+++ fumadores.py
@@ -1,54 +1,44 @@
-import threading
-import time
+
+from threading import Thread, Semaphore
 import random
 
-class Agent(threading.Thread): #clase agente
-    def __init__(self, sem_ingrediente, sem_agente): #constructor
-        threading.Thread.__init__(self) #constructor de la clase padre
-        self.sem_ingrediente = sem_ingrediente  #semaforo de los ingredientes
-        self.sem_agente = sem_agente    #semaforo del agente
-    def run(self):  
-        while True: #ciclo infinito
-            self.sem_agente.acquire()   #semaforo del agente
-            print("\nEl agente esta poniendo los ingredientes") 
-            time.sleep(1)   
-            self.sem_ingrediente[random.randint(0,4)].release() #semaforo de los ingredientes
-            self.sem_ingrediente[random.randint(0,4)].release()
-            self.sem_ingrediente[random.randint(0,4)].release()
-            self.sem_ingrediente[random.randint(0,4)].release()
-            self.sem_ingrediente[random.randint(0,4)].release()
+# Ingredients
+paper = Semaphore(1)
+tobacco = Semaphore(1)
+matches = Semaphore(1)
 
-            print("\nEl agente ha puesto los ingredientes")
-            time.sleep(1)
-            self.sem_fumador.release()  #semaforo del fumador
-            
-class Smoker(threading.Thread): #clase fumador
-    def __init__(self, id, sem_ingrediente, sem_fumador, sem_agente): #constructor
-        threading.Thread.__init__(self) #constructor de la clase padre
-        self.id = id    #id del fumador
-        self.sem_ingrediente = sem_ingrediente  #semaforo de los ingredientes
-        self.sem_fumador = sem_fumador  #semaforo del fumador
-        self.sem_agente = sem_agente    #semaforo del agente
-    def run(self):
-        while True: #ciclo infinito
-            self.sem_ingrediente[self.id].acquire() #semaforo de los ingredientes
-            print("\nEl fumador ", self.id, " esta fumando")
-            time.sleep(1)
-            self.sem_agente.release()   #semaforo del agente
-    
-def main():
-    sem_ingrediente = [threading.Semaphore(0), threading.Semaphore(0), threading.Semaphore(0), threading.Semaphore(0), threading.Semaphore(0)] #semaforos para los ingredientes
-    sem_fumador = threading.Semaphore(0) #semaforo para el fumador
-    sem_agente = threading.Semaphore(0)  #semaforo para el agente
-    hilo_agente = Agent(sem_ingrediente, sem_agente)   #creacion del hilo del agente
-    hilo_fumador = [Smoker(0, sem_ingrediente, sem_fumador, sem_agente), Smoker(1, sem_ingrediente, sem_fumador, sem_agente), Smoker(2, sem_ingrediente, sem_fumador, sem_agente), Smoker(3, sem_ingrediente, sem_fumador, sem_agente), Smoker(4, sem_ingrediente, sem_fumador, sem_agente)] #creacion de los hilos de los fumadores
-    hilo_agente.start() #inicializacion del hilo del agente
-    hilo_fumador[0].start() #inicializacion del hilo del fumador
-    hilo_fumador[1].start()
-    hilo_fumador[2].start()
-    hilo_fumador[3].start()
-    hilo_fumador[4].start()
-    sem_agente.release()    #semaforo del agente
-    
-if __name__ == "__main__":
-    main()
+# Agent Semaphore
+agent_sem = Semaphore(1)
+
+# Smoker Semaphore
+smoker_sem = Semaphore(0)
+
+# Helper function to simulate smoking
+def smoker_func(resource1, resource2):
+    while True:
+        resource1.acquire()
+        resource2.acquire()
+        print("Smoker is smoking...")
+        agent_sem.release()
+
+# Create smokers
+Thread(target=smoker_func, args=(paper, tobacco)).start()
+Thread(target=smoker_func, args=(tobacco, matches)).start()
+Thread(target=smoker_func, args=(paper, matches)).start()
+
+# Agent thread
+def agent_func():
+    while True:
+        agent_sem.acquire()
+        resources = [paper, tobacco, matches]
+        a, b = random.sample(resources, 2)
+        print("Agent places", a, "and", b)
+        if a == paper and b == tobacco:
+            matches.release()
+        elif a == tobacco and b == matches:
+            paper.release()
+        else:
+            tobacco.release()
+        smoker_sem.release()
+
+Thread(target=agent_func).start()
